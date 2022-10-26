8ac0b50ceef21fd4cdd89806109507e55dd569ba
o0-James-0o <jamestaylor1010@hotmail.com>
2022-10-26 14:47:22 -0300

    voltando para o estado do paulo

--- app.js
+++ app.js
@@ -2,4 +2,3 @@ console.log("Rodando o sistema de gerenciamentos de jogos");
 console.log("a aplicação está se conectando ao banco de dados");
 //mais um console log de teste:
 console.log("O Paulo fez esse commit sem voce ver na aula!");
-console.log("Enviado para o GitHub");
