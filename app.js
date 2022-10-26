2d81b2ed2f82422b30e3494da8edf00e66e887cb
o0-James-0o <jamestaylor1010@hotmail.com>
2022-10-26 13:52:11 -0300

    adicionando um console log

--- app.js
+++ app.js
@@ -2,3 +2,4 @@ console.log("Rodando o sistema de gerenciamentos de jogos");
 console.log("a aplicação está se conectando ao banco de dados");
 //mais um console log de teste:
 console.log("O Paulo fez esse commit sem voce ver na aula!");
+console.log("Enviado para o GitHub");
